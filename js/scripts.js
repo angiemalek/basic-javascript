c3c71d0cdff82b88c855151ac7a781ea38e8866e
angiemalek <angiemalek@gmail.com>
2022-03-18 08:06:03 -0500

    Update scripts.js
    add pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello World!');
+let pokemonList = [
+  {
+    name: 'bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass,' 'poison']
+  },
 
-let favoriteFood = 'bananas';
-document.write(favoriteFood);
+  {
+    name: 'weedle',
+    height: 0.3,
+    weight: 3.2,
+    type: ['bug', 'poison']
+  },
+
+  {
+    name: 'jigglypuff',
+    height: 0.5,
+    weight: 5.5,
+    type: ['fairy', 'normal']
+  },
+
+  {
+    name: 'zubat',
+    height: 0.8,
+    weight: 7.5,
+    type: ['poison', 'flying']
+  },
+
+  {
+    name: 'exeggcute',
+    height: 0.4,
+    weight: 2.5,
+    type: ['psychic', 'grass']
+  },
+]
