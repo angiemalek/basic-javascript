7882dd811e1c11bcb2043283f0dbe6f9de2981c6
angiemalek <angiemalek@gmail.com>
2022-04-05 09:47:11 -0500

    Update scripts.js
    add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -61,6 +61,9 @@ let pokemonRepository = (function() {
         button.classList.add('button-class');
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
+        button.addEventListener('click', function(){
+          showDetails(pokemon);
+        })
       }
 
   function showDetails(pokemon){
