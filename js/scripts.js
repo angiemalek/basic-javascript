824e432b276fa1b8365431b552c4fa61b6c01f78
angiemalek <angiemalek@gmail.com>
2022-04-09 18:16:09 -0500

    Update scripts.js
    add load details function

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,21 @@ let pokemonRepository = (function() {
       });
     }
 
+    //Load Pokemon details
+    function loadDetails(item) {
+      let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then (function (details) {
+          // Adds details to Pokemons
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+       });
+      }
+
 
 
   function showDetails(pokemon){
